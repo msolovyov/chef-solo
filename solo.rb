9c92c764b9251f43634bfc5b051feb090b7d898b
Michael Solovyov <spinalfluid@gmail.com>
2011-12-16 16:25:58 -0800

    updates to use nginx and rvm

--- solo.rb
+++ solo.rb
@@ -1,7 +1,7 @@
 root = File.absolute_path(File.dirname(__FILE__))
  
 file_cache_path root
-cookbook_path root + '/cookbooks'
+cookbook_path [root + '/cookbooks', '/home/michael/dev/chef/cookbooks']
 role_path root + "/roles"
 log_level :info
 log_location STDOUT
