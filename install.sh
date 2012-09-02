bf6387b456b4d25a29577ffd050537bceb579e78
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2012-09-03 06:58:12 +0900

    modify script to work with MacOSX

--- install.sh
+++ install.sh
@@ -1,47 +1,96 @@
 #!/bin/bash
 
+# [ $(id -u) == 0 ] || { echo "Must be run as root or with sudo"; exit 1; }
+
 json="${1}"
 
-logfile="/root/chef-solo.log"
+OS=$(uname -s)
 
-# This runs as root on the server
-chef_binary="/usr/local/rvm/gems/ruby-1.9.3-p0/bin/chef-solo"
+if [ $OS == 'Darwin' ]; then
+    logfile="/var/root/chef-solo.log"
+    chef_binary="${HOME}/.rvm/gems/ruby-1.9.3-p194/bin/chef-solo"
+    RVM="${HOME}/.rvm/bin/rvm"
+    
+else
+
+    logfile="/root/chef-solo.log"
+    # This runs as root on the server
+    chef_binary="/usr/local/rvm/gems/ruby-1.9.3-p0/bin/chef-solo"
+    RVM="/usr/local/rvm/scripts/rvm"
+fi
+
+
+
 
 
 # Are we on a vanilla system?
 if ! test -f "$chef_binary"; then
 
-     export DEBIAN_FRONTEND=noninteractive
+    case $OS in
 
-     # Upgrade headlessly (this is only safe-ish on vanilla systems)
-     apt-get update -o Acquire::http::No-Cache=True
-     apt-get -o Dpkg::Options::="--force-confnew" \
-         --force-yes -fuy dist-upgrade
+        "Darwin")
+            # In MacOSX RVM installed as a non-root into ~/.rvm directory
+            bash < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )
 
-     # Install RVM as root (System-wide install)
-     apt-get install -y build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion
-     apt-get install libqt4-dev libqtwebkit-dev
-     # Note system-wide installs are not in the RVM main version
-     sudo bash < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )
-     sudo usermod -a -G rvm ubuntu #add the ubuntu user to the rvm group
-     
-     (cat <<'EOP'
+            #
+            # Install Home brew
+            #
+            which  brew > /dev/null || ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
+
+            brew install git openssl automake subversion lzlib homebrew/dupes/zlib
+            #
+            # TODO: Do we need to add ubuntu (or another) user?
+            #
+            # sudo dseditgroup -o edit -a usernametoadd -t user admin
+            # sudo dseditgroup -o edit -a usernametoadd -t user wheel
+
+            # end Darwin
+        ;;
+        
+        "Ubuntu")
+            
+            export DEBIAN_FRONTEND=noninteractive
+            
+            # Upgrade headlessly (this is only safe-ish on vanilla systems)
+            apt-get update -o Acquire::http::No-Cache=True
+            apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy dist-upgrade
+            
+            # Install RVM as root (System-wide install)
+            apt-get install -y build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion
+
+            apt-get install libqt4-dev libqtwebkit-dev
+
+            sudo bash < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )
+            sudo usermod -a -G rvm ubuntu #add the ubuntu user to the rvm group
+
+            (cat <<'EOP'
 [[ -s "/usr/local/rvm/scripts/rvm" ]] && source "/usr/local/rvm/scripts/rvm" # This loads RVM into a shell session.
 EOP
-     ) > /etc/profile.d/rvm.sh
+        ) > /etc/profile.d/rvm.sh
 
-     # Install Ruby using RVM
-     [[ -s "/usr/local/rvm/scripts/rvm" ]] && source "/usr/local/rvm/scripts/rvm"
-     rvm install ruby-1.9.3-p0
-     rvm use ruby-1.9.3-p0 --default
+            # end Ubuntu
+            ;;
 
-
-     
-     # Install chef
-     gem install --no-rdoc --no-ri chef --version 0.10.0
+        *)
+            echo "Don't know this OS: $OS"
+            exit 1
+            ;;
+    esac
+    
+    # Note system-wide installs are not in the RVM main version
+    
+    
+    
+    # Install Ruby using RVM
+    [[ -s $RVM ]] && source $RVM
+    rvm install ruby-1.9.3-p0
+    rvm use ruby-1.9.3-p0 --default
+    
+    # Install chef
+    gem install --no-rdoc --no-ri chef --version 0.10.0
     gem install --no-rdoc --no-ri bundler 
 fi
 
 # Run chef-solo on server
-[[ -s "/usr/local/rvm/scripts/rvm" ]] && source "/usr/local/rvm/scripts/rvm"
+[[ -s  $RVM ]] && source $RVM
 "$chef_binary" --config solo.rb --json-attributes "$json"
