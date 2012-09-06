d5703cdf0e912b472b11825783bfa20458c6e05b
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2012-09-06 20:38:45 +0900

    move logic from json file to roles: monitoring

--- roles/monitoring.rb
+++ roles/monitoring.rb
@@ -1,5 +1,11 @@
  name "monitoring"
  description "The monitoring server"
  run_list(
-          "recipe[munin::server]"
+          "recipe[munin::server]",
+          "recipe[monit]",
+          "recipe[monit-nginx]",
+          "recipe[monit-unicorn]",
+          "recipe[munin::server]",
+          "recipe[munin::client]",
+          "recipe[munin::unicorn]"
           )
