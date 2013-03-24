03382dca01f155579ff026833ef7d181ab9e9c6f
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2013-03-24 09:36:23 +0900

    Add elastic search recipe and role (with java), tm_staging JSON file

--- roles/redis.rb
+++ roles/redis.rb
@@ -3,7 +3,8 @@ description "Redis key-value store"
 
 default_attributes :redis => {
   "daemonize" => "yes",
-  "pidfile"   => "/var/run/redis/redis-server.pid"
+  "pidfile"   => "/var/run/redis/redis-server.pid",
+  'config_path'  => "/etc/redis.conf"
 }
 
 run_list ["recipe[redis]"] 
