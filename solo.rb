9c7f79709a69c6383beb196a165d908bfb8666f3
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2013-05-20 12:12:43 +0900

    Remove extra path for cookbooks.

--- solo.rb
+++ solo.rb
@@ -1,7 +1,7 @@
 root = File.absolute_path(File.dirname(__FILE__))
  
 file_cache_path root
-cookbook_path [root + '/cookbooks', '/home/michael/dev/chef/cookbooks', root + '/site-cookbooks']
+cookbook_path [root + '/cookbooks', root + '/site-cookbooks']
 role_path root + "/roles"
 data_bag_path root + "/data_bags"
 log_level :info
