29e213fcd5e662a61e664d2fd47f8d262b59123f
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2013-05-20 12:12:33 +0900

    Remove unused code.

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 # Copies files to the server and runs install.
 
-#play 'bork.wav' 2>/dev/null &
-
-#host="${1:-map7@192.168.200.161}"
 host="${1}"
-json="${2}"
+json="${2:-empty.json}"
 
 if [ -z "$host" ]; then
     echo "Usage: ./deploy.sh [user@host] [json]"
     echo "\nEG: ./deploy.sh fred@192.168.1.1 web_server.json"
-    echo "If no json is given it will default to solo.json"
+    echo "If no json is given it will default to epmty.json"
     exit
 fi
 
-if [ -z "$json" ]; then
-    json="webserver.json"
-fi
 
 # The host key might change when we instantiate a new VM, so
 # we remove (-R) the old host key from known_hosts
@@ -26,12 +20,3 @@ ssh-keygen -R "${host#*@}" 2> /dev/null
 echo 'copy chef dir & run install.sh'
 RSYNC_RSH="ssh -o 'StrictHostKeyChecking no'" rsync -ar --delete . ${host}:~/chef
 ssh -t -o 'StrictHostKeyChecking no' "$host" " cd ~/chef && sudo bash install.sh $json"
-
-
-# echo 'copy chef dir & run install.sh'
-# tar cj . | ssh -o 'StrictHostKeyChecking no' "$host" "
-# sudo rm -rf ~/chef &&
-# mkdir ~/chef &&
-# cd ~/chef &&
-# tar xj &&
-# sudo bash install.sh $json"
